1dcabe03b92d25fbdd6c03f72306ee331703a424
kim <saugkim@users.noreply.github.com>
2022-03-08 08:53:09 +0200

    osa2 task 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,23 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
-import Person from './components/Person'
-import Button from './components/Button'
-import Input from './components/Input'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
+
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    // { name: 'Arto Hellas', number: '040-123456' },
+    // { name: 'Ada Lovelace', number: '39-44-5323523' },
+    // { name: 'Dan Abramov', number: '12-43-234345' },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -57,17 +66,13 @@ const App = () => {
         <Input text='name' name={newName} handler={handleNameChange} />
         <Input text='number' name={newNumber} handler={handleNumberChange} />
         <Button text='add' handler={addPerson} />
-      </form>
-       */}
+      </form> */}
 
       <Header text='Numbers' />
       <Persons persons={persons} filter={newFilter} />
       {/* <table><tbody>
         {persons.map( person =>
            <Person key={person.name} name={person.name} number={person.number} />
-        )}
-        {persons.filter(person => person.name.toLowerCase().startsWith(newFilter.toLowerCase())).map(p => 
-           <Person key={p.name} name={p.name} number={p.number} />
         )}
       </tbody></table> */}
     </div>
